813bba4efb799ba97448641cf268e4d8f6db1c42
kouhei19971023 <kouhei19971023aiueo@gmail.com>
2024-01-29 17:43:17 +0900

    画面遷移の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'messages/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   devise_for :users
-  root to: "messages#index"
+  root to: "rooms#index"
   resources :users, only: [:edit, :update]
   resources :rooms, only: [:new, :create]
 end
